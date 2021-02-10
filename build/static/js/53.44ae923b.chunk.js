2a3b260acada4d7a23dfa8f61993f1cc8681f206
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-02-10 15:23:43 +0530

    Capacitor  is added

---
+++ build/static/js/53.44ae923b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprate-mate"]=this["webpackJsonprate-mate"]||[]).push([[53],{137:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return i}));var o=e(12),i=function(){var t=window;t.addEventListener("statusTap",(function(){Object(o.h)((function(){var n=t.innerWidth,e=t.innerHeight,i=document.elementFromPoint(n/2,e/2);if(i){var r=i.closest("ion-content");r&&r.componentOnReady().then((function(){Object(o.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.44ae923b.chunk.js.map
