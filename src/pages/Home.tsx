5e7765b91cf0ffd78159bd75f16145220ee7b5da
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-02-10 10:37:50 +0530

    basic work load

---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
+import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Map map</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+
+      <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
+    </IonPage>
+  );
+};
+
+export default Home;
