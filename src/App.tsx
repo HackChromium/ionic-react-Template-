1be2cc71fecc3a7d48c8228a044da2f33a78e7ea
nitz-prnv <nitprnv@gmail.com>
2021-02-11 08:32:01 +0530

    added geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
-
+import Test from './pages/Test'
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -32,6 +32,7 @@ const App: React.FC = () => (
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
+        <Route exact path='/test' component={Test} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
